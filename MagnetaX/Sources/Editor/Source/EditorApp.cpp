ccf606c4611f42760a8bb0d84ec15de9cbf39890
szymonkrygier <skprimedev@gmail.com>
2026-08-29 23:43:48 +0200

    Updated ImGUI integrations (experimental)

--- MagnetaX/Sources/Editor/Source/EditorApp.cpp
+++ MagnetaX/Sources/Editor/Source/EditorApp.cpp
@@ -14,6 +14,132 @@
 #include <imgui.h>
 #include <memory>
 
+namespace
+{
+    // Temp place, I will move this
+    // to other file later...
+    ImGuiKey ToImGuiKey(KeyboardKeys key)
+    {
+        switch (key)
+        {
+        // Verify later if correct, generated!
+        case KeyboardKeys::TAB: return ImGuiKey_Tab;
+        case KeyboardKeys::ARROW_LEFT: return ImGuiKey_LeftArrow;
+        case KeyboardKeys::ARROW_RIGHT: return ImGuiKey_RightArrow;
+        case KeyboardKeys::ARROW_UP: return ImGuiKey_UpArrow;
+        case KeyboardKeys::ARROW_DOWN: return ImGuiKey_DownArrow;
+        case KeyboardKeys::PAGE_UP: return ImGuiKey_PageUp;
+        case KeyboardKeys::PAGE_DOWN: return ImGuiKey_PageDown;
+        case KeyboardKeys::HOME: return ImGuiKey_Home;
+        case KeyboardKeys::END: return ImGuiKey_End;
+        case KeyboardKeys::INSERT: return ImGuiKey_Insert;
+        case KeyboardKeys::DEL: return ImGuiKey_Delete;
+        case KeyboardKeys::BACKSPACE: return ImGuiKey_Backspace;
+        case KeyboardKeys::SPACE: return ImGuiKey_Space;
+        case KeyboardKeys::ENTER: return ImGuiKey_Enter;
+        case KeyboardKeys::ESCAPE: return ImGuiKey_Escape;
+
+        case KeyboardKeys::LEFT_CTRL: return ImGuiKey_LeftCtrl;
+        case KeyboardKeys::LEFT_SHIFT: return ImGuiKey_LeftShift;
+        case KeyboardKeys::LEFT_ALT: return ImGuiKey_LeftAlt;
+        case KeyboardKeys::LEFT_SUPER: return ImGuiKey_LeftSuper;
+        case KeyboardKeys::RIGHT_CTRL: return ImGuiKey_RightCtrl;
+        case KeyboardKeys::RIGHT_SHIFT: return ImGuiKey_RightShift;
+        case KeyboardKeys::RIGHT_ALT: return ImGuiKey_RightAlt;
+        case KeyboardKeys::RIGHT_SUPER: return ImGuiKey_RightSuper;
+
+        case KeyboardKeys::A: return ImGuiKey_A;
+        case KeyboardKeys::B: return ImGuiKey_B;
+        case KeyboardKeys::C: return ImGuiKey_C;
+        case KeyboardKeys::D: return ImGuiKey_D;
+        case KeyboardKeys::E: return ImGuiKey_E;
+        case KeyboardKeys::F: return ImGuiKey_F;
+        case KeyboardKeys::G: return ImGuiKey_G;
+        case KeyboardKeys::H: return ImGuiKey_H;
+        case KeyboardKeys::I: return ImGuiKey_I;
+        case KeyboardKeys::J: return ImGuiKey_J;
+        case KeyboardKeys::K: return ImGuiKey_K;
+        case KeyboardKeys::L: return ImGuiKey_L;
+        case KeyboardKeys::M: return ImGuiKey_M;
+        case KeyboardKeys::N: return ImGuiKey_N;
+        case KeyboardKeys::O: return ImGuiKey_O;
+        case KeyboardKeys::P: return ImGuiKey_P;
+        case KeyboardKeys::Q: return ImGuiKey_Q;
+        case KeyboardKeys::R: return ImGuiKey_R;
+        case KeyboardKeys::S: return ImGuiKey_S;
+        case KeyboardKeys::T: return ImGuiKey_T;
+        case KeyboardKeys::U: return ImGuiKey_U;
+        case KeyboardKeys::V: return ImGuiKey_V;
+        case KeyboardKeys::W: return ImGuiKey_W;
+        case KeyboardKeys::X: return ImGuiKey_X;
+        case KeyboardKeys::Y: return ImGuiKey_Y;
+        case KeyboardKeys::Z: return ImGuiKey_Z;
+
+        case KeyboardKeys::DIGIT_0: return ImGuiKey_0;
+        case KeyboardKeys::DIGIT_1: return ImGuiKey_1;
+        case KeyboardKeys::DIGIT_2: return ImGuiKey_2;
+        case KeyboardKeys::DIGIT_3: return ImGuiKey_3;
+        case KeyboardKeys::DIGIT_4: return ImGuiKey_4;
+        case KeyboardKeys::DIGIT_5: return ImGuiKey_5;
+        case KeyboardKeys::DIGIT_6: return ImGuiKey_6;
+        case KeyboardKeys::DIGIT_7: return ImGuiKey_7;
+        case KeyboardKeys::DIGIT_8: return ImGuiKey_8;
+        case KeyboardKeys::DIGIT_9: return ImGuiKey_9;
+
+        case KeyboardKeys::F1: return ImGuiKey_F1;
+        case KeyboardKeys::F2: return ImGuiKey_F2;
+        case KeyboardKeys::F3: return ImGuiKey_F3;
+        case KeyboardKeys::F4: return ImGuiKey_F4;
+        case KeyboardKeys::F5: return ImGuiKey_F5;
+        case KeyboardKeys::F6: return ImGuiKey_F6;
+        case KeyboardKeys::F7: return ImGuiKey_F7;
+        case KeyboardKeys::F8: return ImGuiKey_F8;
+        case KeyboardKeys::F9: return ImGuiKey_F9;
+        case KeyboardKeys::F10: return ImGuiKey_F10;
+        case KeyboardKeys::F11: return ImGuiKey_F11;
+        case KeyboardKeys::F12: return ImGuiKey_F12;
+
+        case KeyboardKeys::APOSTROPHE: return ImGuiKey_Apostrophe;
+        case KeyboardKeys::COMMA: return ImGuiKey_Comma;
+        case KeyboardKeys::MINUS: return ImGuiKey_Minus;
+        case KeyboardKeys::PERIOD: return ImGuiKey_Period;
+        case KeyboardKeys::SLASH: return ImGuiKey_Slash;
+        case KeyboardKeys::SEMICOLON: return ImGuiKey_Semicolon;
+        case KeyboardKeys::EQUAL: return ImGuiKey_Equal;
+        case KeyboardKeys::LEFT_BRACKET: return ImGuiKey_LeftBracket;
+        case KeyboardKeys::BACKSLASH: return ImGuiKey_Backslash;
+        case KeyboardKeys::RIGHT_BRACKET: return ImGuiKey_RightBracket;
+        case KeyboardKeys::GRAVE: return ImGuiKey_GraveAccent;
+
+        case KeyboardKeys::CAPS_LOCK: return ImGuiKey_CapsLock;
+        case KeyboardKeys::SCROLL_LOCK: return ImGuiKey_ScrollLock;
+        case KeyboardKeys::NUM_LOCK: return ImGuiKey_NumLock;
+        case KeyboardKeys::PRINT_SCREEN: return ImGuiKey_PrintScreen;
+        case KeyboardKeys::PAUSE: return ImGuiKey_Pause;
+
+        case KeyboardKeys::NUMPAD_0: return ImGuiKey_Keypad0;
+        case KeyboardKeys::NUMPAD_1: return ImGuiKey_Keypad1;
+        case KeyboardKeys::NUMPAD_2: return ImGuiKey_Keypad2;
+        case KeyboardKeys::NUMPAD_3: return ImGuiKey_Keypad3;
+        case KeyboardKeys::NUMPAD_4: return ImGuiKey_Keypad4;
+        case KeyboardKeys::NUMPAD_5: return ImGuiKey_Keypad5;
+        case KeyboardKeys::NUMPAD_6: return ImGuiKey_Keypad6;
+        case KeyboardKeys::NUMPAD_7: return ImGuiKey_Keypad7;
+        case KeyboardKeys::NUMPAD_8: return ImGuiKey_Keypad8;
+        case KeyboardKeys::NUMPAD_9: return ImGuiKey_Keypad9;
+        case KeyboardKeys::NUMPAD_DECIMAL: return ImGuiKey_KeypadDecimal;
+        case KeyboardKeys::NUMPAD_DIVIDE: return ImGuiKey_KeypadDivide;
+        case KeyboardKeys::NUMPAD_MULTIPLY: return ImGuiKey_KeypadMultiply;
+        case KeyboardKeys::NUMPAD_SUBTRACT: return ImGuiKey_KeypadSubtract;
+        case KeyboardKeys::NUMPAD_ADD: return ImGuiKey_KeypadAdd;
+        case KeyboardKeys::NUMPAD_ENTER: return ImGuiKey_KeypadEnter;
+
+        default:
+            return ImGuiKey_None;
+        }
+    }
+}
+
 struct EditorApp::EditorAppImpl
 {
     std::unique_ptr<AbstractWindow> editorWindow;
@@ -66,6 +192,13 @@ struct EditorApp::EditorAppImpl
 
         ImGuiIO& io = ImGui::GetIO();
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.BackendPlatformName = "MagnetaX";
+
+        unsigned char* fontPixels = nullptr;
+        int32 fontWidth = 0;
+        int32 fontHeight = 0;
+
+        io.Fonts->GetTexDataAsRGBA32(&fontPixels, &fontWidth, &fontHeight);
 
         clock.Reset();
 
@@ -79,12 +212,14 @@ struct EditorApp::EditorAppImpl
 
     void Tick()
     {
+        clock.Update();
+
         inputSystem->BeginFrame();
         editorWindow->PollEvents();
 
         BeginImGuiFrame();
 
-        //ImGui::EndFrame();
+        ImGui::EndFrame();
 
         UIRenderData uiData{};
         graphicsSystem->RenderScene(&editorScene, nullptr, uiData);
@@ -147,7 +282,36 @@ struct EditorApp::EditorAppImpl
             io.AddMouseWheelEvent(0.0f, (float32)wheelDelta / 120.0f);
         }
 
-        //ImGui::NewFrame();
+        UpdateImGuiKeyboard();
+
+        ImGui::NewFrame();
+    }
+
+    void UpdateImGuiKeyboard()
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        const Input::Keyboard keyboard = inputSystem->GetKeyboard();
+
+        for (usize i = (usize)KeyboardKeys::UNKNOWN + 1; i < (usize)KeyboardKeys::COUNT; ++i)
+        {
+            const KeyboardKeys key = (KeyboardKeys)i;
+            const ImGuiKey imguiKey = ToImGuiKey(key);
+
+            if (imguiKey == ImGuiKey_None) continue;
+
+            if (keyboard.KeyPressed(key)) io.AddKeyEvent(imguiKey, true);
+            if (keyboard.KeyReleased(key)) io.AddKeyEvent(imguiKey, false);
+        }
+
+        const bool ctrl = keyboard.KeyDown(KeyboardKeys::LEFT_CTRL) || keyboard.KeyDown(KeyboardKeys::RIGHT_CTRL);
+        const bool shift = keyboard.KeyDown(KeyboardKeys::LEFT_SHIFT) || keyboard.KeyDown(KeyboardKeys::RIGHT_SHIFT);
+        const bool alt = keyboard.KeyDown(KeyboardKeys::LEFT_ALT) || keyboard.KeyDown(KeyboardKeys::RIGHT_ALT);
+        const bool super = keyboard.KeyDown(KeyboardKeys::LEFT_SUPER) || keyboard.KeyDown(KeyboardKeys::RIGHT_SUPER);
+
+        io.AddKeyEvent(ImGuiMod_Ctrl, ctrl);
+        io.AddKeyEvent(ImGuiMod_Shift, shift);
+        io.AddKeyEvent(ImGuiMod_Alt, alt);
+        io.AddKeyEvent(ImGuiMod_Super, super);
     }
 
     void HandleWindowEvent(const WindowEvent& event)
@@ -163,14 +327,30 @@ struct EditorApp::EditorAppImpl
 
             break;
         }
-
         case WindowEventType::CLOSING:
         {
             wantsExit = true;
 
             break;
         }
+        case WindowEventType::FOCUS_GAINED:
+        {
+            if (ImGui::GetCurrentContext())
+            {
+                ImGui::GetIO().AddFocusEvent(true);
+            }
 
+            break;
+        }
+        case WindowEventType::FOCUS_LOST:
+        {
+            if (ImGui::GetCurrentContext())
+            {
+                ImGui::GetIO().AddFocusEvent(false);
+            }
+
+            break;
+        }
         default:
             break;
         }
