d97d606a2049abec276e1dc65d0808ed69a20b9c
szymonkrygier <skprimedev@gmail.com>
2026-08-31 22:36:55 +0200

    Buffering ScriptComponents before running them in case they get deleted during runtime

--- MagnetaX/Sources/Engine/Source/Scripting/ScriptSystem.cpp
+++ MagnetaX/Sources/Engine/Source/Scripting/ScriptSystem.cpp
@@ -4,41 +4,52 @@
 #include <MX/EngineContext.h>
 #include <MX/Scene/Scene.h>
 #include <MX/Scene/Component/ScriptComponent.h>
+#include <vector>
 
 void ScriptSystem::Update(Scene& scene, EngineContext& engineContext, float64 deltaTime)
 {
+    std::vector<Entity> entities;
+
     scene.ForEach<ScriptComponent>(
-        [&](Entity entity, ScriptComponent& scriptComponent)
+        [&](Entity entity, ScriptComponent&)
         {
-            const usize scriptCount = scriptComponent.scripts.size();
+            entities.push_back(entity);
+        }
+    );
 
-            for (usize i = 0; i < scriptCount; ++i)
+    for (Entity entity : entities)
+    {
+        ScriptComponent* scriptComponent = entity.GetComponent<ScriptComponent>();
+        if (!scriptComponent) continue;
+
+        const usize scriptCount = scriptComponent->scripts.size();
+
+        for (usize i = 0; i < scriptCount; ++i)
+        {
+            ScriptComponent* currentComponent = entity.GetComponent<ScriptComponent>();
+            if (!currentComponent || i >= currentComponent->scripts.size()) break;
+
+            std::unique_ptr<Script> script = std::move(currentComponent->scripts[i]);
+            if (!script) continue;
+
+            script->entity = entity;
+            script->engineContext = &engineContext;
+
+            if (!script->started)
             {
-                ScriptComponent* currentComponent = entity.GetComponent<ScriptComponent>();
-                if (!currentComponent || i >= currentComponent->scripts.size()) break;
-
-                std::unique_ptr<Script> script = std::move(currentComponent->scripts[i]);
-                if (!script) continue;
-
-                script->entity = entity;
-                script->engineContext = &engineContext;
-
-                if (!script->started)
-                {
-                    script->OnStart();
-                    script->started = true;
-
-                    currentComponent = entity.GetComponent<ScriptComponent>();
-                    if (!currentComponent || i >= currentComponent->scripts.size() || currentComponent->scripts[i]) continue;
-                }
-
-                script->OnUpdate(deltaTime);
+                script->OnStart();
+                script->started = true;
 
                 currentComponent = entity.GetComponent<ScriptComponent>();
                 if (!currentComponent || i >= currentComponent->scripts.size() || currentComponent->scripts[i]) continue;
-
-                currentComponent->scripts[i] = std::move(script);
             }
+
+            script->OnUpdate(deltaTime);
+
+            currentComponent = entity.GetComponent<ScriptComponent>();
+            if (!currentComponent || i >= currentComponent->scripts.size() || currentComponent->scripts[i]) continue;
+
+            currentComponent->scripts[i] = std::move(script);
         }
-    );
+    }
 }
